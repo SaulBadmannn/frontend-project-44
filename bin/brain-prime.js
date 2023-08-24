35a27258778892398cb5d36657a50a1ffd0f8e59
SaulBadmannn <samat7089@gmail.com>
2023-08-24 21:47:11 +0500

    Make ninth step

---
+++ bin/brain-prime.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const targetAmountCorrectAnswers = 3;
+let userAmountCorrectAnswers = 0;
+let randomNumber = 0;
+const maxRandomNumber = 20;
+const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19];
+let correctAnswer;
+let playerAnswer;
+
+const playerName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${playerName}!`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+for (let index = 0; index < targetAmountCorrectAnswers; index++) {
+    randomNumber = Math.floor(Math.random() * maxRandomNumber + 1);
+
+    primeNumbers.includes(randomNumber) ? correctAnswer = 'yes' : correctAnswer = 'no';
+    
+    console.log(`Question: ${randomNumber}`);
+    playerAnswer = readlineSync.question('Your answer: ');
+
+    if (playerAnswer === correctAnswer) {
+        console.log('Correct!');
+        userAmountCorrectAnswers += 1;
+    } else {
+        console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`)
+        break;
+    }
+}
+
+if (userAmountCorrectAnswers === targetAmountCorrectAnswers) {
+    console.log(`Congratulations, ${playerName}!`)
+}
