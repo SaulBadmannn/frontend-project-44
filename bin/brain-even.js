fe83845bb5142638e625093cac381c842121b0d8
SaulBadmannn <samat7089@gmail.com>
2023-08-30 22:37:43 +0500

    Make remake of games

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,38 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import greetingPlayer from '../src/cli.js';
 
-const targetAmountCorrectAnswers = 3;
-let userAmountCorrectAnswers = 0;
-let randomNumber = 0;
-const maxRandomNumber = 100;
-let correctAnswer;
-let playerAnswer;
-
-const playerName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${playerName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-for (let index = 0; index < targetAmountCorrectAnswers; index += 1) {
-  randomNumber = Math.floor(Math.random() * maxRandomNumber + 1);
-
-  if (randomNumber % 2 === 0) {
-    correctAnswer = 'yes';
-  } else {
-    correctAnswer = 'no';
-  }
-
-  console.log(`Question: ${randomNumber}`);
-  playerAnswer = readlineSync.question('Your answer: ');
-
-  if (playerAnswer === correctAnswer) {
-    console.log('Correct!');
-    userAmountCorrectAnswers += 1;
-  } else {
-    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`);
-    break;
-  }
-}
-
-if (userAmountCorrectAnswers === targetAmountCorrectAnswers) {
-  console.log(`Congratulations, ${playerName}!`);
-}
+greetingPlayer();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greetingPlayer = () => {
+  console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 };
